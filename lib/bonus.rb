750325dd05f9935aeada062fbc5872f0a02f6aa5
Adam Urbina <adamurbina@gmail.com>
2018-06-01 20:19:11 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,8 @@ def bonus
 
   #code your solution here:
 
-  [:montague][:hero][:status] = "dead"
-  [:capulet][:heroin][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroin][:status] = "dead"
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
